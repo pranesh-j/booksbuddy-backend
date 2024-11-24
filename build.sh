2b97a288655c9f29dea244374f14505ee4553db9
pranesh-j <praneshjahagirdar8888@gmail.com>
2024-11-24 14:14:36 +0530

    Add build script for Render deployment

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Python dependencies
+echo "Building the project..."
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+# Run database migrations
+echo "Running database migrations..."
+python manage.py migrate 
